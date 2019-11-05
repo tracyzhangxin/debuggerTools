36c991c8f71d5314c3ba8a5301e0a3a8fd948665
Bin <1510120026@vip.henu.edu.cn>
2019-11-05 14:00:11 +0800

    尝试使用xlwt 包实现自动填充excel 报告

--- present/sortUI.py
+++ present/sortUI.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from utils import CoverageUtil, JudgeResult, ResultStorge
 import os
+import xlwt
+
 
 def printProgress(iteration, total, prefix='', suffix='', decimals=1, barLength=100):
     """
@@ -23,6 +25,40 @@ def printProgress(iteration, total, prefix='', suffix='', decimals=1, barLength=
         sys.stdout.write('\n')
     sys.stdout.flush()
 
+
+# 设置表格样式
+def set_style(name, height, bold=False):
+    style = xlwt.XFStyle()
+    font = xlwt.Font()
+    font.name = name
+    font.bold = bold
+    font.color_index = 4
+    font.height = height
+    style.font = font
+    return style
+
+
+# 写Excel
+def write_excel():
+    f = xlwt.Workbook() #创建工作簿
+    sheet1 = f.add_sheet('学生', cell_overwrite_ok=True)#创建工作表
+    row0 = ["姓名", "年龄", "出生日期", "爱好"] #标题栏
+    colum0 = ["张三", "李四", "恋习Python", "小明", "小红", "无名"]
+    # 写第一行
+    for i in range(0, len(row0)):
+        sheet1.write(0, i, row0[i], set_style('Times New Roman', 220, True))
+    # 写第一列
+    for i in range(0, len(colum0)):
+        sheet1.write(i + 1, 0, colum0[i], set_style('Times New Roman', 220, True))
+
+    sheet1.write(1, 3, '2006/12/12')
+    sheet1.write_merge(6, 6, 1, 3, '未知')  # 合并行单元格
+    sheet1.write_merge(1, 2, 3, 3, '打游戏')  # 合并列单元格
+    sheet1.write_merge(4, 5, 3, 3, '打篮球')
+
+    f.save('test.xls')
+
+
 def generateReport(suspiciousnessRank):
     count = 0
     check = 0
@@ -33,16 +69,18 @@ def generateReport(suspiciousnessRank):
         print("You must provide a test suite containing both successful abd failed test cases.")
     else:
         print("The most suspicious line:\n\tLine Rank")
-        if len(suspiciousnessRank) >= 10:
-            for item in suspiciousnessRank:
-                count += 1
-                print("\t{:<5}{:<5}".format(item[0],count))
-                # if count == 20:
-                #     break
-        else:
-            for item in suspiciousnessRank:
-                count += 1
-                print("\t{:<5}{:<5}".format(item[0],count))
+        # if len(suspiciousnessRank) >= 10:
+        for item in suspiciousnessRank:
+            count += 1
+            print("\t{:<5}{:<5}".format(item[0], count))
+            # if count == 20:
+            #     break
+        # else:
+        #     for item in suspiciousnessRank:
+        #         count += 1
+        #         print("\t{:<5}{:<5}".format(item[0],count))
+    return suspiciousnessRank;
+
 
 def main():
     absolutepath = os.path.abspath("../")
@@ -53,7 +91,6 @@ def main():
     type = raw_input("please input the debugger algorithm: 1:Tarantula; 2:CrossTab; 3:Jaccard : ")
     subcode = raw_input("please input the sort code name: ")
     sourcecode = sourcecode + subcode
-
 
     if not os.path.isfile(sourcecode):
         print(sourcecode + "is not file")
@@ -74,7 +111,8 @@ def main():
         # print(filename)
 
         resultStorge.setFilePath(testSuite + "/" + filename)
-        record = coverageUtil.calCoverLine(testSuite + "/" + filename, testSuite + "/" + filename.replace(".in", ".out"))
+        record = coverageUtil.calCoverLine(testSuite + "/" + filename,
+                                           testSuite + "/" + filename.replace(".in", ".out"))
         result = judgeResult.check(testSuite + "/" + filename, testSuite + "/" + filename.replace(".in", ".out"))
         resultStorge.addrecord(result, record)
         os.remove(testSuite + "/" + filename.replace(".in", ".out"))
@@ -85,13 +123,9 @@ def main():
     coverageUtil.clear()
     coverageList = coverageUtil.getLineCoverage(coverageUtil.lines, resultStorge.records)
     resultlist = resultStorge.rankBySuspiciousness(coverageList)
-    print('==============')
-    print(resultlist)
-    print('==============')
-    generateReport(resultlist)
-
+    print(resultlist)  # 所有测试集的结果
+    suspiciousnessRank = generateReport(resultlist)  # 生成排名的函数
 
 
 if __name__ == '__main__':
     main()
-
